017de8fbb44869366fd77dd8b24aaab0243f046b
Ratzmx <enzoxz.023@gmail.com>
2025-03-20 11:15:01 -0300

    Initial commit

---
+++ src/main/java/cl/ratzmx/porcentaje/PercentageApplication.java
@@ -0,0 +1,13 @@
+package cl.ratzmx.porcentaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PercentageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PercentageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/ratzmx/porcentaje/PorcentajeApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ratzmx.porcentaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorcentajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
